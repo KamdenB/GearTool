d1ffde64fec2e7f3ccb1e068ea79d62ac1893b23
KamdenBolhuis <kamden.bolhuis@gmail.com>
2020-05-25 19:02:38 -0400

    Inital commit

--- calculate.js
+++ calculate.js
@@ -3,42 +3,67 @@ let maxTeeth = 84
 let increment = .1
 let currentPitch = .9
 let currentDiameter = 1
+/*
+Generates all gears with their tooth count, pitch, and diameter
+*/
 let data = [];
-for(let i = minTeeth; i<=maxTeeth; i+=2){
-    let teeth = i;
-    let pitch = parseFloat(currentPitch.toString().substring(0,4));
-    let diameter = parseFloat(currentDiameter.toString().substring(0,4));
-    data.push({teeth,pitch,diameter})
-    currentPitch+=increment;
-    currentDiameter+=increment;
+for (let i = 18; i <= 84; i += 2) {
+    let pitch = parseFloat(currentPitch.toString().substring(0, 4));
+    let diameter = parseFloat(currentDiameter.toString().substring(0, 4));
+    data.push({ teeth:i, pitch, diameter });
+    currentPitch += increment;
+    currentDiameter += increment;
 }
 
-let centerToCenter = 4.5;
+/*
+    Returns the overall gear ratio for the two inputted gears
+*/
+let gearRatio = (g1, g2) => {
+    return g1>g2 ? g1/g2 : g2/g1;
+}
 
-let combinations = (ctc) => {
+let getPitch = (gear) => {
+    return (data.filter((p) => { return p.teeth == gear})).pitch;
+}
+
+let tolerance = (number, tolerance) => {
+    // if(number < number+tolerance && number+tolerance !< number+(tolerance*2) )
+}
+
+/* 
+    Based off the center to center distance of the two gears, it willprovide 
+    all possible gear ratios.
+*/
+let combinations = (ctc, variance=0) => {
     let foundRatios = [];
-    for(let i = 0; i < data.length; i++){
-        let currentP = data[i].pitch;
-        for(let x = 0; x < data.length; x++){
-            if(currentP + data[x].pitch == ctc){
-                let ratio = parseFloat((data[i].teeth/data[x].teeth).toString().substring(0,3))
-                foundRatios.push({gear1:data[i].teeth,gear2:data[x].teeth,ratio:`${ratio}:1`})
+    for (let i in data) {
+        for (let x in data) {
+            if (data[i].pitch + data[x].pitch == ctc) {
+            // if (data[i].pitch + data[x].pitch == ctc || ((data[i].pitch + data[x].pitch)+variance)) {
+                let ratio = parseFloat(gearRatio(data[i].teeth,data[x].teeth).toString().substring(0, 3))
+                foundRatios.push({ gear1: data[i].teeth, gear2: data[x].teeth, ratio: `${ratio}:1`, ctc })
             }
         }
     }
+    // Remove all duplicate ratios
+    foundRatios = [...new Map(foundRatios.map(item => [item.ratio, item])).values()]
+   
     return foundRatios;
 }
+
+
+//////////////////////////////////////////////////
+
 
 let gear1 = 18;
 let gear2 = 72;
 
-let p1 = data.filter((g) => {return g.teeth == gear1});
-let p2 = data.filter((g) => {return g.teeth == gear2});
+let p1 = data.filter((g) => { return g.teeth == gear1 });
+let p2 = data.filter((g) => { return g.teeth == gear2 });
 
-let totalPitch = p1[0].pitch+p2[0].pitch;
+let totalPitch = p1[0].pitch + p2[0].pitch;
 
 let combinationsRes = combinations(totalPitch);
-
 
 console.log(p1[0].pitch, p2[0].pitch, totalPitch)
 console.log(combinationsRes)
